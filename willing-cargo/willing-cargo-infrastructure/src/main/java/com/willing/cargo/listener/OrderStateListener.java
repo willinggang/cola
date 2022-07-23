40b39bce86ef4b8ea083402faee316b25128b87d
sunzhigang <864307818@qq.com>
2022-07-23 22:56:22 +0800

    添加状态机监听器

---
+++ willing-cargo/willing-cargo-infrastructure/src/main/java/com/willing/cargo/listener/OrderStateListener.java
@@ -0,0 +1,28 @@
+package com.willing.cargo.listener;
+
+import com.willing.cargo.enume.OrderStatusChangeEvent;
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.annotation.OnTransition;
+import org.springframework.statemachine.annotation.OnTransitionEnd;
+import org.springframework.statemachine.annotation.OnTransitionStart;
+import org.springframework.statemachine.annotation.WithStateMachine;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Szg
+ * @ClassName:
+ * @Description:
+ * @date 2022/7/2322:51
+ */
+@Component
+@WithStateMachine(id = "orderStateMachineId")
+public class OrderStateListener {
+
+
+    @OnTransition(source = "",target = "")
+    @OnTransitionEnd
+    @OnTransitionStart
+    public void payTransition(Message<OrderStatusChangeEvent> message){
+
+    }
+}
